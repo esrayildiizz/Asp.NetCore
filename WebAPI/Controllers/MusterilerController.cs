74a7b09b6c70f414f8bc2a667c5bd88e6544ca62
Esra Yıldız <esrayildiizz@outlook.com>
2022-12-26 13:51:39 +0300

    Controller eklendi.

---
+++ WebAPI/Controllers/MusterilerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusterilerController : ControllerBase
+    {
+        private IMusterilerRepository musterilerRepository;
+
+        private IWebHostEnvironment webHostEnvironment;
+
+
+        public MusterilerController(IMusterilerRepository repo, IWebHostEnvironment environment)
+        {
+            musterilerRepository = repo;
+            webHostEnvironment = environment;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Musteriler> GetMusteriler()
+        {
+            return musterilerRepository.GetAllMusteriler().ToList();
+        }
+
+        [HttpGet("id")]
+        public Musteriler GetMusteriById(int id)
+        {
+            return musterilerRepository.GetMusteriById(id); 
+        }
+
+        [HttpPost]
+        public Musteriler Create([FromBody] Musteriler musteriler)
+        {
+            return musterilerRepository.AddMusteri(musteriler);
+        }
+
+        [HttpPut]
+        public Musteriler Update([FromForm] Musteriler musteriler)
+        {
+            return musterilerRepository.UpdateMusteri(musteriler);
+        }
+
+        [HttpDelete]
+        public void Delete(int? id) => musterilerRepository.DeleteMusteri(id);
+
+    }
+}
