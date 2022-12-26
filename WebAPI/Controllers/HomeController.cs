e48aa9883f335e6a4708e1faa141f63c7ff8f7f1
Esra Yıldız <esrayildiizz@outlook.com>
2022-12-26 14:23:50 +0300

    Home controller mvc olarak acildi.

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "API Running....";
+        }
+    }
+}
