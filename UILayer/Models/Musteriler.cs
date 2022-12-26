71ee2404f3de7cd9d34100e773d32583a1d1dafa
Esra Yıldız <esrayildiizz@outlook.com>
2022-12-26 14:23:29 +0300

    Mvc katmani olusturdum ve duzenlemeler yapildi.

---
+++ UILayer/Models/Musteriler.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UILayer.Models
+{
+    public class Musteriler
+    {
+
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Lütfen isim giriniz")]
+        public string AdSoyad { get; set; }
+
+        [Required(ErrorMessage = "Lütfen adres giriniz")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Lütfen telefon giriniz")]
+        public string Telefon { get; set; }
+
+        [Required(ErrorMessage = "Lütfen email giriniz")]
+        public string Email { get; set; }
+    }
+}
